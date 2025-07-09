e45158c40eee16a835290b5fd50472bcbc072f45
Zakaria El Baghazaoui <zakaria.baghazaoui@gmail.com>
2025-07-09 18:25:00 +0100

    Changing the border color of shadcn components to black

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+import { Check } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (<>
     <Button>Bismi Lah</Button>
     <p className="text-rose-500">Alhamduli Lah</p>
+    <Input placeholder="Enter your name" />
+    <Textarea placeholder="Enter your message" />
+    <Checkbox/>
+    <Progress value={50} className="w-full" />
     </>
   );
 }
