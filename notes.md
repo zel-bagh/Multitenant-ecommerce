d509677ccfb860a695490687da28afba4aaa8238
Zakaria El Baghazaoui <zakaria.baghazaoui@gmail.com>
2025-07-10 15:28:57 +0100

    Adding notes for later

---
+++ notes.md
@@ -0,0 +1,4 @@
+# Things to investigate later:
+# - Css!
+# - Typescript!
+# - Next navigation!
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { Progress } from "@/components/ui/progress";
 import { Textarea } from "@/components/ui/textarea";
-import { Check } from "lucide-react";
-import Image from "next/image";
 
 export default function Home() {
 
