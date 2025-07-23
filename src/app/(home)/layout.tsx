b2ca3874864fbbad4074526605bc15a21aec4ec1
Zakaria El Baghazaoui <zakaria.baghazaoui@gmail.com>
2025-07-23 19:45:27 +0100

    (homePage: footer

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "./footer";
 import { Navbar } from "./navbar";
 
 interface Props {
@@ -8,7 +9,10 @@ const Layout = ({ children }: Props) => {
   return (
     <div className="flex flex-col min-h-screen">
         <Navbar />
-        {children}
+        <div className="flex-1 bg-[#F4F4F0]">
+          {children}
+        </div>
+        <Footer />
     </div>
   );
 }
